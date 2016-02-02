9f62d82d280553832b869d8bc93b3d05140a1c77
jonnyjack <jonnyjack7@gmail.com>
2016-02-01 23:54:56 -0500

    Finished Project Euler 238... but there is room for improvement.  I'd love to see if it would ever finish haha.

--- ProjectEuler_238.py
+++ ProjectEuler_238.py
@@ -1,93 +1,85 @@
 from itertools import tee
 from pdb import set_trace
+import functools
 
-def getNextNumber(n):
-  return (n*n) % 20300713
-  
-digitGetters = []
-def getCachedDigitGetters(idx, getDigitGetter):
-  if idx-1 < len(digitGetters):
-    return digitGetters[idx-1]
-  elif idx-1 == len(digitGetters):
-    return next(getDigitGetter)
-  else:
-    raise ValueError("cannot get further ahead than next digitGetter")
+class _state:
+  def __init__(self):
+    self.lastSum = {}
+    self.sumGenerators = {}
+    
+  def getPK(self):
+    def naturalGetter():
+      i=0
+      while True:
+        i += 1;
+        yield i
+        
+    kGetter = naturalGetter()
+    while True:
+      k=next(kGetter)
+      #set_trace()
+      idx=0
+      foundPK = False
+      while not foundPK:
+        idx += 1
+        if idx in self.lastSum:
+          if k in self.lastSum[idx]:
+            yield idx; break
+          elif k < self.lastSum[idx][-1]:
+            continue
+        sums = getSumGetter(idx,self)
+        for sum in sums:
+          if sum==k:
+            foundPK = True;
+            yield idx;
+            break
+          elif sum>k:
+            break
 
-def getDigitGetters():
-  digitGetters.append(getDigits())
-  yield digitGetters[-1]
-  it = getDigits()
-  for d in it:
-    _, it2 = tee(it)
-    digitGetters.append(it2)
-    yield it2
+def sumGetter(idx, state):
+  getter = getDigitGetter(idx)
+  sum = 0
+  for d in getter:
+    if d != 0:
+      sum += d
+      if idx in state.lastSum:
+        state.lastSum[idx].append(sum)
+        if len(state.lastSum[idx])>10: state.lastSum[idx].pop(0)
+      else:
+        state.lastSum[idx] = [sum]
+      yield sum
+
+def getSumGetter(idx, state):
+  if idx in state.sumGenerators:
+    return state.sumGenerators[idx]
+  state.sumGenerators[idx] = sumGetter(idx, state)
+  return state.sumGenerators[idx]
+
+def getDigitGetter(idx):
+  getter = getDigits()
+  for i in range(idx-1):
+    next(getter)
+  return getter
   
 def getDigits():
+  def getNextNumber(n):
+    return (n*n) % 20300713
+
   s0 = s = "14025256"
   while True:
-    for ch in s:
-      yield int(ch)
+    for ch in s: yield int(ch)
     s = str(getNextNumber(int(s)))
-    if s == s0:
-      break;
+    if s == s0: break
       
 def calculateSumOfPksUpTo(n):
-  def shrink_gap(idx):
-    nonlocal p_k, min_k, p_k_sum, max_sz_p_k, num_found
-    getter = getCachedDigitGetters(idx, getDigitGetter)
-    sum = 0
-    for d in getter:
-      if d != 0:
-        sum += d
-        if sum > n: break
-        if sum == min_k+1:
-          set_trace()
-          num_found += 1
-          p_k.add(sum)
-          while min_k+1 in p_k:
-            if min_k in p_k:
-              p_k.remove(min_k)
-            min_k += 1
-          p_k_sum += idx
-          if len(p_k) <= max_sz_p_k/2:
-            return min_k
-          if num_found == n: break
-        elif sum > min_k+1:
-          return shrink_gap(idx+1)
-    
-  p_k = set()
-  p_k_sum = 0
-  min_k = 0
-  max_k = 0
-  max_sz_p_k = 10
-  num_found = 0
-  idx = 1
-  getDigitGetter = getDigitGetters()
-  while (len(p_k)<n):
-    getter = getCachedDigitGetters(idx, getDigitGetter)
-    sum = 0
-    for d in getter:
-      if d != 0:
-        sum += d
-        if sum > n: break
-        if sum > min_k and sum not in p_k:
-          set_trace()
-          num_found += 1
-          p_k.add(sum)
-          if sum > max_k:
-            max_k = sum
-          while min_k+1 in p_k:
-            if min_k in p_k:
-              p_k.remove(min_k)
-            min_k += 1
-          p_k_sum += idx
-          if len(p_k) == max_sz_p_k:
-            min_k = shrink_gap(idx)
-          if num_found == n: break
-            
-    if num_found == n: break
-    idx += 1
-  return p_k_sum
+  sum = 0
+  found = 0
+  state = _state()
+  for pk in state.getPK():
+    found += 1
+    sum += pk
+    if found==n: return sum
+
 
 def calculateSumOfPks():
   return calculateSumOfPksUpTo(2*10**15)
