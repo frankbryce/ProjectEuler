522fee7daae3cb3009b3b568631696bc2d672e82
jonnyjack <jonnyjack7@gmail.com>
2016-02-02 22:58:35 -0500

    I need to figure out an efficient way to find sums of prime numbers which are prime

--- ProjectEuler_249.py
+++ ProjectEuler_249.py
@@ -1,20 +1,31 @@
 from sys import argv
 from math import sqrt
 from pdb import set_trace
+import functools
 
+@functools.lru_cache(maxsize=65536)
 def isPrime(n):
   for i in getPrimesUpTo(int(sqrt(n))):
     if n%i==0: return False
   return True
 
+def odds(endingAt, startingAt=3):
+  i=startingAt
+  while i<=endingAt:
+    yield i
+    i+=2
+  
 def getPrimesUpTo(n):
-  yield 2; yield 3
-  if n<=3: return
-  for i in list(range(n+1))[3::2]:
+  yield 2
+  if n<=2: return
+  for i in odds(n):
     if isPrime(i): yield i
+    if i+2>n: break;
 
 def getPrimeSubsetSumsUpTo(n):
-  pass
+  for u in getPrimeUpTo(n):
+    sum = u
+    if isPrime(sum): yield sum
 
 if __name__=="__main__":
   ps = list(getPrimesUpTo(int(argv[1])))
---
+++ test_ProjectEuler_249.py
@@ -0,0 +1,14 @@
+from ProjectEuler_249 import getPrimesUpTo
+import pytest
+
+@pytest.mark.parametrize("n",[(1000,168)])
+def test_itGetsAllPrimes(n):
+  assert len(list(getPrimesUpTo(n[0])))==n[1]
+  
+@pytest.mark.parametrize("n",[(2,2),(3,3),(4,3),(1000,997),(10000,9973)])
+def test_itGetsCorrectPrimes(n):
+  assert list(getPrimesUpTo(n[0]))[-1]==n[1]
+  
+@pytest.mark.parametrize("n",[10,1000,10000])
+def test_itsFastToo(benchmark, n):
+  benchmark(getPrimesUpTo, n)
