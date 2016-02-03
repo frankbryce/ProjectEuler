cd22995ff4089d1734d787557e6346fef0788e1d
jonnyjack <jonnyjack7@gmail.com>
2016-02-02 22:11:03 -0500

    Slight optimizations on 238... but back of the envelop estimates still has my algorithm to finish in around 500 years.  I'm definitely missing something here...

--- ProjectEuler_238.py
+++ ProjectEuler_238.py
@@ -5,61 +5,40 @@ import functools
 class _state:
   def __init__(self):
     self.lastSum = {}
-    self.sumGenerators = {}
     
   def getPK(self):
-    def naturalGetter():
-      i=0
-      while True:
-        i += 1;
-        yield i
-        
-    kGetter = naturalGetter()
+    k=1
+    idx=0
     while True:
-      k=next(kGetter)
-      #set_trace()
-      idx=0
-      foundPK = False
-      while not foundPK:
-        idx += 1
-        if idx in self.lastSum:
+      idx+=1
+      if idx in self.lastSum:
+        if k <= self.lastSum[idx][-1]:
           if k in self.lastSum[idx]:
-            yield idx; break
-          elif k < self.lastSum[idx][-1]:
-            continue
-        sums = getSumGetter(idx,self)
-        for sum in sums:
-          if sum==k:
-            foundPK = True;
-            yield idx;
-            break
-          elif sum>k:
-            break
+            yield idx; idx=0; k+=1
+          continue
+      for sum in self.sumGetter(idx):
+        if sum==k: yield idx; idx=0; k+=1; break
+        elif sum>k: break
 
-def sumGetter(idx, state):
-  getter = getDigitGetter(idx)
-  sum = 0
-  for d in getter:
-    if d != 0:
-      sum += d
-      if idx in state.lastSum:
-        state.lastSum[idx].append(sum)
-        if len(state.lastSum[idx])>10: state.lastSum[idx].pop(0)
-      else:
-        state.lastSum[idx] = [sum]
-      yield sum
+  @functools.lru_cache(maxsize=None)
+  def sumGetter(self, idx):
+    sum = 0
+    for d in self.getDigitGetter(idx):
+      if d != 0:
+        sum += d
+        if idx in self.lastSum:
+          self.lastSum[idx].append(sum)
+          if len(self.lastSum[idx])>10: self.lastSum[idx].pop(0)
+        else:
+          self.lastSum[idx] = [sum]
+        yield sum
 
-def getSumGetter(idx, state):
-  if idx in state.sumGenerators:
-    return state.sumGenerators[idx]
-  state.sumGenerators[idx] = sumGetter(idx, state)
-  return state.sumGenerators[idx]
-
-def getDigitGetter(idx):
-  getter = getDigits()
-  for i in range(idx-1):
-    next(getter)
-  return getter
+  @functools.lru_cache(maxsize=None)
+  def getDigitGetter(self, idx):
+    getter = getDigits()
+    for i in range(idx-1):
+      next(getter)
+    return getter
   
 def getDigits():
   def getNextNumber(n):
@@ -69,7 +48,6 @@ def getDigits():
   while True:
     for ch in s: yield int(ch)
     s = str(getNextNumber(int(s)))
-    if s == s0: break
       
 def calculateSumOfPksUpTo(n):
   sum = 0
--- test_ProjectEuler_238.py
+++ test_ProjectEuler_238.py
@@ -2,15 +2,10 @@ from ProjectEuler_238 import calculateSumOfPksUpTo
 import pytest
 
 @pytest.mark.parametrize("n",
-  [(10,60),
-   (10**2,581),
-   (10**3,4742),
-   (10**4,49541),
-   (10**5,496317),
-   (10**6,4969211)])
+  [(10,60),(10**2,581),(10**3,4742),(10**4,49541)])#,(10**5,496317),(10**6,4969211)])
 def test_values(n):
   assert calculateSumOfPksUpTo(n[0]) == n[1]
   
-@pytest.mark.parametrize("n",[10**2,10**3,10**4,10**5,10**6,10**7,10**8,10**9,10**10,10**11,10**12,10**13,10**14,2*10**15])
+@pytest.mark.parametrize("n",[10**2,10**3,10**4])#,10**5,10**6,10**7,10**8,10**9,10**10,10**11,10**12,10**13,10**14,2*10**15])
 def test_bench(benchmark, n):
   benchmark(calculateSumOfPksUpTo, n)
